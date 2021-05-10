fe63deb5ad9288eea9ecf31b7e98d543fc1a5727
Nikhil Dabhade <nikhilkd1999@gmail.com>
2021-05-10 19:16:02 +0530

    Updated UI

--- lib/home_page_screen.dart
+++ lib/home_page_screen.dart
@@ -21,121 +21,123 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('XO'),
       ),
-      body: Container(
-        width: double.infinity,
-        // color: winner=="" ? Colors.white : Colors.black,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (!isBotEnabled)
-              Container(
-                margin: EdgeInsets.all(5),
-                child: Text(
-                  getWinner(LST) == ""
-                      ? (Xchance ? "X's Turn" : "O's Turn")
-                      : "",
-                  style: TextStyle(fontSize: 20),
+      body: Center(
+        child: Container(
+          width: double.infinity,
+          // color: winner=="" ? Colors.white : Colors.black,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (!isBotEnabled)
+                Container(
+                  margin: EdgeInsets.all(5),
+                  child: Text(
+                    getWinner(LST) == ""
+                        ? (Xchance ? "X's Turn" : "O's Turn")
+                        : "",
+                    style: TextStyle(fontSize: 20),
+                  ),
                 ),
-              ),
-            Container(
-              height: boxLength * 0.7,
-              width: boxLength * 0.7,
+              Container(
+                height: boxLength,
+                width: boxLength,
 
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(10),
+                // margin: EdgeInsets.all(10),
 
-              child: GridView.count(
-                crossAxisCount: 3,
-                children: LST.asMap().entries.map((entry) {
-                  int ind = entry.key;
-                  // int n = Random();
+                child: GridView.count(
+                  crossAxisCount: 3,
+                  children: LST.asMap().entries.map((entry) {
+                    int ind = entry.key;
+                    // int n = Random();
 
-                  return Container(
-                    // margin: EdgeInsets.all(10),
-                    // height: boxLength*0.1,
-                    // width: boxLength*0.1,
-                    child: InkWell(
-                      onTap: () {
-                        if (LST[ind] != "") {
-                          return;
-                        }
-                        setState(() {
-                          LST[ind] = Xchance ? "X" : "O";
-                          Xchance = !Xchance;
-                          // countt=0;
-                          if (isBotEnabled) {
-                            bestMove(LST);
+                    return Container(
+                      // margin: EdgeInsets.all(10),
+                      // height: boxLength*0.1,
+                      // width: boxLength*0.1,
+                      child: InkWell(
+                        onTap: () {
+                          if (LST[ind] != "") {
+                            return;
                           }
-                          // print(countt);
-                          winner = getWinner(LST);
+                          setState(() {
+                            LST[ind] = Xchance ? "X" : "O";
+                            Xchance = !Xchance;
+                            // countt=0;
+                            if (isBotEnabled) {
+                              bestMove(LST);
+                            }
+                            // print(countt);
+                            winner = getWinner(LST);
 
-                          if (winner != "") {
-                            for (int i = 0; i < 9; ++i) {
-                              if (LST[i] == "") {
-                                LST[i] = "Avni";
+                            if (winner != "") {
+                              for (int i = 0; i < 9; ++i) {
+                                if (LST[i] == "") {
+                                  LST[i] = "Avni";
+                                }
                               }
                             }
-                          }
-                        });
-                      },
-                      splashColor: Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.circular(15),
-                      child: Container(
-                        padding: const EdgeInsets.all(10),
-                        // margin: const EdgeInsets.all(10),
-                        child: Center(
-                          child: LST[ind] == "Avni"
-                              ? Text("")
-                              : Text(
-                                  LST[ind],
-                                  style: TextStyle(
-                                    fontSize: boxLength * 0.3 * 0.5,
-                                    color: Colors.black.withOpacity(0.9),
-                                    // fontWeight: FontWeight.bold,
+                          });
+                        },
+                        splashColor: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(15),
+                        child: Container(
+                          padding: const EdgeInsets.all(10),
+                          // margin: const EdgeInsets.all(10),
+                          child: Center(
+                            child: LST[ind] == "Avni"
+                                ? Text("")
+                                : Text(
+                                    LST[ind],
+                                    style: TextStyle(
+                                      fontSize: boxLength * 0.3 * 0.5,
+                                      color: Colors.black.withOpacity(0.9),
+                                      // fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                ),
-                        ),
+                          ),
 
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                              colors: [
-                                (colorWinner(ind) ? Colors.red : Colors.green)
-                                    .withOpacity(0.7),
-                                (colorWinner(ind) ? Colors.red : Colors.green)
-                              ],
-                              begin: AlignmentDirectional.topStart,
-                              end: AlignmentDirectional.bottomEnd),
-                          borderRadius: BorderRadius.circular(15),
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                                colors: [
+                                  (colorWinner(ind) ? Colors.red : Colors.green)
+                                      .withOpacity(0.7),
+                                  (colorWinner(ind) ? Colors.red : Colors.green)
+                                ],
+                                begin: AlignmentDirectional.topStart,
+                                end: AlignmentDirectional.bottomEnd),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                }).toList(),
-              ),
+                    );
+                  }).toList(),
+                ),
 
-              // child: LST.asMap().map((key, value) {}).toList(),
-            ),
-            if (winner != "")
-              Text(
-                winner == "Tie" ? "Tie" : "Winner is " + winner + " !!",
-                style: TextStyle(fontSize: 30),
+                // child: LST.asMap().map((key, value) {}).toList(),
               ),
-            // if (winner == "")
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              margin: EdgeInsets.all(10),
-              child: ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      reset(LST);
-                    });
-                  },
-                  child: Text("Reset")),
-            )
-          ],
+              if (winner != "")
+                Text(
+                  winner == "Tie" ? "Tie" : "Winner is " + winner + " !!",
+                  style: TextStyle(fontSize: 30),
+                ),
+              // if (winner == "")
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        reset(LST);
+                      });
+                    },
+                    child: Text("Reset")),
+              )
+            ],
+          ),
         ),
       ),
     );
